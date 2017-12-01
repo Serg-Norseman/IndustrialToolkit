7aa986ab5dabb11152c85821c87d7f70428fa941
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2017-12-01 13:00:11 +0300

    Publishing of v1.0

---
+++ MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+using PISDK;
+
+namespace PITagsCleaner
+{
+    public partial class MainForm : Form
+    {
+        private PISDK.PISDK pisdk;
+        private PISDK.Server pisrv;
+        private PISDK.PointList ptList;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            pisdk = new PISDK.PISDK();
+            pisrv = pisdk.Servers.DefaultServer;
+            pisrv.Open();
+        }
+
+        void btnTagsView_Click(object sender, EventArgs e)
+        {
+            ptList = pisrv.GetPoints("tag='" + tbFilter.Text + "'");
+
+            ProgressBar.Minimum = 0;
+            ProgressBar.Maximum = ptList.Count;
+            ProgressBar.Value = 0;
+
+            lbTags.Items.Clear();
+            foreach (PIPoint p in ptList) {
+                lbTags.Items.Add(p.Name);
+                ProgressBar.Increment(1);
+            }
+
+            ProgressBar.Value = 0;
+        }
+
+        void btnClear_Click(object sender, EventArgs e)
+        {
+            ptList = pisrv.GetPoints("tag='" + tbFilter.Text + "'");
+
+            ProgressBar.Minimum = 0;
+            ProgressBar.Maximum = ptList.Count;
+            ProgressBar.Value = 0;
+
+            int i = 0;
+
+            lbTags.Items.Clear();
+            foreach (PIPoint p in ptList) {
+                i++;
+                this.Text = i.ToString() + " / " + ptList.Count.ToString();
+				
+                lbTags.Items.Add(p.Name);
+				
+                try {
+                    p.Data.RemoveValues(dtpStart.Value, dtpFinish.Value, DataRemovalConstants.drRemoveAll);
+                } catch (Exception ex) {
+                    textBox1.AppendText(ex.Message.ToString() + " [" + p.Name + ", " + i.ToString() + "]\n");
+                }
+				
+                ProgressBar.Increment(1);
+                Application.DoEvents();
+            }
+
+            ProgressBar.Value = 0;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PITagsCleaner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PITagsCleaner")]
+[assembly: AssemblyCopyright("Copyright © 2012 Sergey V. Zhdanovskih")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
