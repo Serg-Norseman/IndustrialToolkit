3ec3530c062a5daf0dd50570facc9372b491ad9b
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2018-02-16 10:07:03 +0300

    Preparing PIBrowser for merging into PIToolkit

--- clean.cmd
+++
@@ -1,8 +0,0 @@
-rmdir .\.vs /s /q
-
-rmdir .\PIBrowser\bin /s /q
-rmdir .\PIBrowser\obj /s /q
-
-rmdir .\PIBrowserTests\bin /s /q
-rmdir .\PIBrowserTests\obj /s /q
-rmdir .\PIBrowserTests\OpenCover /s /q
