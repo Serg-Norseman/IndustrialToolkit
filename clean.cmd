0cd70e2d4b6c77cfcd099a7ac7c9d7b378fcb51f
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2018-02-16 10:29:47 +0300

    Unification (2)

--- clean.cmd
+++ clean.cmd
@@ -5,3 +5,10 @@ rmdir .\AFEventsCleaner\obj /s /q
 
 rmdir .\PITagsCleaner\bin /s /q
 rmdir .\PITagsCleaner\obj /s /q
+
+rmdir .\PIBrowser\bin /s /q
+rmdir .\PIBrowser\obj /s /q
+
+rmdir .\PIBrowserTests\bin /s /q
+rmdir .\PIBrowserTests\obj /s /q
+rmdir .\PIBrowserTests\OpenCover /s /q
